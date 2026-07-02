80bb06ab86e5cb597a570cae23899aca9891d06a
JI YEON KIM <jiyeeon1208@gmail.com>
2026-07-03 02:32:51 +0900

    결제처리 프로세스 작업중2
    결제처리 프로세스 작업중2

--- backend/MIPS/src/main/java/com/mips/domain/payment/repository/PaymentRepository.java
+++ backend/MIPS/src/main/java/com/mips/domain/payment/repository/PaymentRepository.java
@@ -4,5 +4,10 @@ import com.mips.domain.charge.entity.Charge;
 import com.mips.domain.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Optional<Payment> findByPaymentId(String paymentId);
+
 }
--- backend/MIPS/src/main/java/com/mips/domain/payment/service/PaymentService.java
+++ backend/MIPS/src/main/java/com/mips/domain/payment/service/PaymentService.java
@@ -4,10 +4,15 @@ import com.mips.domain.charge.entity.Charge;
 import com.mips.domain.charge.repository.ChargeRepository;
 import com.mips.domain.payment.dto.PaymentRequest;
 import com.mips.domain.payment.dto.PortOneResponse;
+import com.mips.domain.payment.entity.Payment;
 import com.mips.domain.payment.entity.PaymentRawLog;
 import com.mips.domain.payment.enums.PaymentStatus;
+import com.mips.domain.payment.enums.SelectedChannelType;
 import com.mips.domain.payment.repository.PaymentRawLogRepository;
+import com.mips.domain.payment.repository.PaymentRepository;
 import com.mips.global.config.PortOneSecretProperties;
+import io.portone.sdk.server.errors.PaymentNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
@@ -19,6 +24,7 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class PaymentService {
 
@@ -26,15 +32,19 @@ public class PaymentService {
     private String webhookVerifier;
     private final PaymentRawLogRepository paymentRawLogRepository;
     private final ChargeRepository chargeRepository;
+    private final PaymentRepository paymentRepository;
 
     public PaymentService(@Autowired @Qualifier("portoneRestClient") RestClient portoneRestClient,
                           PortOneSecretProperties properties,
                           PaymentRawLogRepository paymentRawLogRepository,
-                          ChargeRepository chargeRepository) {
+                          ChargeRepository chargeRepository,
+                          PaymentRepository paymentRepository
+    ) {
         this.portoneRestClient = portoneRestClient;
         this.webhookVerifier = properties.getWebhook();
         this.paymentRawLogRepository = paymentRawLogRepository;
         this.chargeRepository = chargeRepository;
+        this.paymentRepository = paymentRepository;
     }
 
 
@@ -51,32 +61,48 @@ public class PaymentService {
         paymentRawLogRepository.save(prl);
 
 
-        // 2. Payment에 update
+        // 2. 포트원에 결제층전 요청내역 처리(단건) 조회
         PortOneResponse resPayment = portoneRestClient.get()
                             .uri("/payments/{paymentId}?storeId={storeId}", request.getPaymentId(), request.getStoreId())
                             .accept(MediaType.APPLICATION_JSON)
                             .retrieve()
                             .body(PortOneResponse.class);
 
+        if (resPayment == null) {
+            log.error("Could not retrieve PortOneResponse from PortOne");
+            throw new NullPointerException();
+        }
          // 3. Charge에 update
         Optional<Charge> charge = chargeRepository.findByChargeId(request.getChargeId());
 
         charge.ifPresent(c -> {
-            if (resPayment != null) {
-                // status가 지불일때
-                if (resPayment.getStatus().equals(PaymentStatus.PAID.name())) {
-                    c.setPaidAt(LocalDateTime.ofInstant(resPayment.getPaidPayment().getPaidAt(), ZoneId.of("Asia/Seoul")));
-                }
-                else if (resPayment.getStatus().equals(PaymentStatus.CANCELLED.name())) {
-                    c.setCanceledAt(LocalDateTime.ofInstant(resPayment.getCancelledPayment().getCancelledAt(), ZoneId.of("Asia/Seoul")));
-                }
-                c.setTransactionId(resPayment.getTransactionId());
-                c.setStatus(PaymentStatus.valueOf(resPayment.getStatus()));
+            c.setTransactionId(resPayment.getTransactionId());
+            c.setStatus(PaymentStatus.valueOf(resPayment.getStatus()));
+
+            // status가 지불일 때
+            if (resPayment.getStatus().equals(PaymentStatus.PAID.name())) {
+                c.setPaidAt(LocalDateTime.ofInstant(resPayment.getPaidPayment().getPaidAt(), ZoneId.of("Asia/Seoul")));
             }
+            // status가 취소일 때
+            else if (resPayment.getStatus().equals(PaymentStatus.CANCELLED.name())) {
+                c.setCanceledAt(LocalDateTime.ofInstant(resPayment.getCancelledPayment().getCancelledAt(), ZoneId.of("Asia/Seoul")));
+            }
+
             chargeRepository.save(c);
         });
 
+        // 4. Payment에 update
+        Optional<Payment> payment = paymentRepository.findByPaymentId(request.getPaymentId());
+        payment.ifPresent( p -> {
+            p.setTransactionId(resPayment.getTransactionId());
+            p.setStatus(PaymentStatus.valueOf(resPayment.getStatus()));
+            p.setBillingKey(resPayment.getBillingKey());
+            p.setVersion(resPayment.getVersion().getValue());
+            p.setChannel(SelectedChannelType.valueOf(resPayment.getChannel().getType().getValue()));
+            p.setChannelGroup(resPayment.getChannelGroup().getId());
+            paymentRepository.save(p);
+        });
 
-        return null;
+        return resPayment;
     }
 }
